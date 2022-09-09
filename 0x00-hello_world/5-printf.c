9753760913198745e42bae0b13e6a9f910c3bb34
littlemalcolm <babajideyusuf11@gmail.com>
2022-09-09 01:12:13 +0000

    task6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,22 @@
 /**
  * main - Entry Point
  * no argument
- * Description: prints string to standard output
+ *
+ * Description: prints the bytes for various data types to stdo
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
