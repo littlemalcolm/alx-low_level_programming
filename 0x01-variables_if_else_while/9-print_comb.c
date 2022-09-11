745f4e659423a9f4915e078ec8cb37a1d4892462
littlemalcolm <babajideyusuf11@gmail.com>
2022-09-11 23:08:46 +0000

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints 0 to 9 seperated by a comma and space
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i == 57)
+		break;
+		putchar(44);
+		putchar(32);
+																i++;
+																}
+	putchar(10);
+	return (0);
+}
