02a8baf3eb038dea6d74167192af2bbf8470466b
littlemalcolm <babajideyusuf11@gmail.com>
2022-09-15 03:40:59 +0000

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
-#include <conio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "main.h"
 
 /**
- * main - Write a program that prints _putchar ,
- * followed by a new line
+ * main - print _putchar, followed by new line
  * Return: 0
  */
 
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+		putchar('_');
+		putchar('p');
+		putchar('u');
+		putchar('t');
+		putchar('c');
+		putchar('h');
+		putchar('a');
+		putchar('r');
+		putchar('\n');
 
-	return (0);
+		return (0);
 }
