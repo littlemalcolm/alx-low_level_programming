f4da9d787346c973e06427fba1e16444729d391d
littlemalcolm <babajideyusuf11@gmail.com>
2022-09-21 03:08:31 +0000

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
-#include "main.h"
-
-/**
- * main - print _putchar, followed by new line
- * Return: 0
- */
 
 int main(void)
 {
-		putchar('_');
-		putchar('p');
-		putchar('u');
-		putchar('t');
-		putchar('c');
-		putchar('h');
-		putchar('a');
-		putchar('r');
-		putchar('\n');
-
-		return (0);
-}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,7 @@
  * reset_to_98 - takes a pointer to an int as parameter and updates the value.
  * @c: argument to be checked.
  *
- * Description: Updates the value it points to to 98.
+ * Description: Updates the value it points to.
  * Return: Unspecified reurn value.
  */
 void reset_to_98(int *n)
