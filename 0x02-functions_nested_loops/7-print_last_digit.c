96cf4bea221cf0d13230d4594e3b5d0dad19ce91
littlemalcolm <babajideyusuf11@gmail.com>
2022-12-27 22:52:19 +0000

    task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,25 +7,8 @@
  */
 int print_last_digit(int x)
 {
-	int l, abs_l;
+	if (n < 0)
+		x *= -1;
+	_putchar('0' + (x % 10));
 
-	l = x % 10;
-	abs_l = _abs(1);
-	_putchar(abs_l + 48);
-
-	return (abs_l);
-}
-
-/**
- * _abs - Computes the absolute value of an integer.
- * @x: Integer to be operated on.
- *
- * Return: Abdolute value.
- */
-int _abs(int x);
-{
-	if (x < 0)
-		return (x * -1);
-	else
-		return (x);
-}
+	return (x % 10);
