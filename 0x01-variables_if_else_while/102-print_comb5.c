334187841684bd03c57f00549a513e63d25def57
littlemalcolm <babajideyusuf11@gmail.com>
2023-05-19 23:27:44 +0000

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,11 +21,11 @@ int main(void)
 		{
 			if (num1 < num2)
 			{
-																							putchar((num1 / 10) + 48);
-																							putchar((num1 % 10) + 48);
-																							putchar(32);
-																							putchar((num2 / 10) + 48);
-																							putchar((num2 % 10) + 48);
+				putchar((num1 / 10) + 48);
+																									putchar((num1 % 10) + 48);
+																									putchar(32);
+																									putchar((num2 / 10) + 48);
+																									putchar((num2 % 10) + 48);
 
 					if (num1 != 98 || num2 != 99)
 					{
