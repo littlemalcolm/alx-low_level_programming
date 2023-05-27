37d0bd95b4028a20f66c0cc0fcceb4fde323e8b7
the-YMB <fort_links@yahoo.com>
2023-05-27 15:19:15 +0000

    104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -30,7 +30,7 @@ int main(void)
 
 	for (i = 0; i < 7; i++)
 	{
-		printf(", %lu", (k2 / 1000000000));
+		printf(", %lu", k1 + (k2 / 1000000000));
 		printf("%lu", k2 % 1000000000);
 		k1 = k1 + j1;
 		j1 = k1 - j1;
