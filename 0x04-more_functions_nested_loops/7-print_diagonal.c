aeb4b35cd1647ca29a4957976e49bbbe44413ba8
the-YMB <littemalcolm>
2023-05-28 06:02:11 +0000

    7-print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_diagonal - Draws diagonal line on terminal.
+  * @n: Number of diagonal lines to be drawn.
+  *
+  * Return: Returns no value.
+  */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar(10);
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar(10);
+		}
+	}
+}
