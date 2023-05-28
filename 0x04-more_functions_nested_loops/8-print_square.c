3daa71beba7d1d01d83fb6db4c637b3d0962c85c
the-YMB <littemalcolm>
2023-05-28 06:09:43 +0000

    8-print_square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * print_square - Prints a square of #.
+  * @size: Size of the square.
+  *
+  * Return: No specified return value.
+  */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		x = 1;
+		while (x <= size)
+		{
+			y = 1;
+			while (y <= size)
+			{
+				_putchar('#');
+				y++;
+			}
+			_putchar(10);
+			x++;
+		}
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
