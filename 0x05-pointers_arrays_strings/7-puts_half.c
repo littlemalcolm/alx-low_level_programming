b8bdc00fef013b232027964d0da40ac5f83761ac
the-YMB <littemalcolm>
2023-05-29 23:23:31 +0000

    test7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+  * puts_half - Prints second half of string.
+  * @str: pointer to string.
+  *
+  * Description: If string lenght is odd, (strlen - 1) /2 characters
+  * should be printed.
+  * Return: No specified return value.
+  */
+void puts_half(char *str)
+{
+	int 1;
+
+	if (_strlen(str) % 2 != 0)
+	{
+		for (i = (_strlen(str) + 1) / 2 < _strlen(str); i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = _strlen(str) / 2; i < _strlen(str); i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar(10);
+}
+
+/**
+  * _strlen - Return the length of a string.
+  * @s: Argument pointer of char type.
+  *
+  * Return: Length of a string.
+  */
+int _strlen(char *s)
+{
+	int i, len = 0;
+
+	for (i = 0; 1 ; i++) /* 1 says if True i.e s still exists */
+	{
+		if (*(s + 1) == 0)
+			break;
+		len++;
+	}
+	return (len);
+}
