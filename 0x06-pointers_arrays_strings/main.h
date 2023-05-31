264ee831dac0a0dfcef9b2bdd140f21e28a98d4e
the-YMB <littemalcolm>
2023-05-31 23:42:33 +0000

    1-strncat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
-#ifndef "main.h"
-#define "main.h"
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -11,7 +9,7 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
+int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void rev_string(char *n);
 void print_buffer(char *b, int size);
-
-#endif
