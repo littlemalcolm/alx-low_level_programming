525eda7134e5bac650741be6b7c955946f28a6d7
the-YMB <littemalcolm>
2023-06-08 14:18:38 +0000

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _print_rev_recursion - Prints a string in a reverse.
+  * @s: Pointer to string.
+  *
+  * Return: No specified return value.
+  */
+void _print_rev_recrsion(char *s)
+{
+	int n = 0;
+
+	if (s[n] != 0)
+	{
+		_print_rev_recursion(&s[n + 1]);
+		_putchar(s[n]);
+	}
+}
