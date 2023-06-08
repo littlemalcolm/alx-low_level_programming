6c3996dbd380b5a4654a1f9c93407781e46cb8c2
the-YMB <littemalcolm>
2023-06-08 14:00:17 +0000

    0-puts_recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,12 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
+#include <unistd.h>
+
+int _putchar(char c);
 void _puts_recursion(char *s);
 
 #endif
