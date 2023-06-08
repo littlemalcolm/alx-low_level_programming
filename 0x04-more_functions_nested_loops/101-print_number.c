bcae264a36aeef812e93436a2c47612cd0a900fb
the-YMB <littemalcolm>
2023-06-08 18:39:43 +0000

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_number - Prints an integer.
+  * @n: Integer to be printed.
+  *
+  * Return: No return value.
+  */
+void print_number(int n)
+{
+	unsigned int abs_v = 0;
+
+	if (n >= 0)
+	{
+		abs_v = n;
+	}
+	else
+	{
+		abs_v = n * -1;
+		_putchar('-');
+	}
+	if (abs_v / 10)
+	{
+		print_number(abs_v / 10);
+	}
+	_putchar((abs_v % 10) + 48);
+}
