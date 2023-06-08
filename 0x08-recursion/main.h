9355b67876bca7c29e5d99a8c0808512ca662203
the-YMB <littemalcolm>
2023-06-08 17:35:50 +0000

    6-is_prime_number.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,12 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _sqrt(int n, int i);
 int is_prime_number(int n);
+int check_prime(int n, int p);
+int is_palindrome(char *s);
+int check_match(char *big, char *end);
+int get_len(char *s);
+int wildcmp(char *s1, char *s2);
 
-#endif
+#endif /* for MAIN_H */
