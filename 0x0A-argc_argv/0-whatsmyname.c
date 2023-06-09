ffe14858d23083860bd381c396a098f53292cc00
the-YMB <littemalcolm>
2023-06-09 11:49:12 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * main - Entry point of program.
+  * @argc: Number of arguments passed to main(), including program name.
+  * @argv: Pointer to array of strings, each being an argument passed to main.
+  *
+  * Description: Prints the program name, that is first argument.
+  * Return: 0 if successful.
+  */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	if (1)
+		printf("%s\n", *argv);
+	return (0);
+}
