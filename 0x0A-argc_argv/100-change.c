8e0cdc242e9d377e9ffeebb4c410ee0ecafbfa54
the-YMB <littemalcolm>
2023-06-10 04:40:22 +0000

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,44 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
-  * main - prints the minimum number of coins for an amount of money
-  * @argc: should count two arguments
-  * @argv: arguments given should be program name and amount of money
-  * Return: least number of coins, 0 if negative amount, 1 if amount not given
+  * main - Entry point of program.
+  * @argc: Number of arguments passed to main() including program name.
+  * @argv: Pointer to string arguments.
+  *
+  * Description: Prints minimum number of coins to make change for an amount.
+  * Amount is in cents.
+  * Return: Returns 0 if successful, 1 if negative number is passed
+  * or if more than 1 argument is passed.
   */
+
 int main(int argc, char *argv[])
 {
-	int n, coins = 0;
+	int cents, change;
 
-	/* validate input */
 	if (argc != 2)
 	{
-		{
-			printf("Error\n");
-			return (1);
-		}
+		printf("Error\n");
+		return (1);
+	}
 
-		if (argv[1][0] == '-')
-		{
-			printf("0\n");
-			return (0);
-		}
-
-		/* convert string to int and calculate coins */
-		n = atoi(argv[1]);
-
-		coins += n / 25;
-		n = n % 25;
-		coins += n / 10;
-		n = n % 10;
-		coins += n / 5;
-		n = n % 5;
-		coins += n / 2;
-		n = n % 2;
-		coins += n / 1;
-
-		printf("%d\n", coins);
+	cents = atoi(argv[1]);
+	if (cents < 0)
+	{
+		printf("0\n")
 		return (0);
+	}
+
+	change = change_cents(cents);
+	printf("%d\n", change);
+	return (0);
+}
+/**
+  * change_cents - Calculates minimum number of coins to make change for amount
+  * @cents: Amount in cents.
+  *
+  * Return: Minimum number of coins.
+  */
+int change_cents(int cents)
+{
+	int ch25, ch10, ch5, ch2, ch1;
+
+	ch25 = cents / 25;
+	cents = cents - (ch25 * 25);
+
+	ch10 = cents / 10;
+	cents = cents - (ch10 * 10);
+
+	ch5 = cents / 5;
+	cents = cents - (ch5 * 5);
+
+	ch2 = cents / 2;
+	cents = cents - (ch2 * 2);
+
+	ch1 = cents;
+
+	return (ch25 + ch10 + ch5 + ch2 + ch1);
 }
