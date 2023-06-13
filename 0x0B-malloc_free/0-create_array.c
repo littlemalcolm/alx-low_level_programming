10620c3e5f7b6de7149843698afbf74952e8146e
the-YMB <littemalcolm>
2023-06-13 07:50:44 +0000

    add description of function

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,22 +1,30 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * create_array - create array of chars and initialize with a specific char
+  * @size: size of array
+  * @c: fill array values with this char
+  * Return: pointer to array
+  */
 
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-      	char * bee;
-	
+	char *bee;
+
 	if (size == 0)
 		return (NULL);
-	bee = (char *)malloc(sizeof(char) * size );
+	bee = (char *)malloc(sizeof(char) * size);
 	if (bee == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 		bee[i] = c;
 	bee[i] = 0;
-	return bee;
+	return (bee);
 
 
 
-} 
+}
