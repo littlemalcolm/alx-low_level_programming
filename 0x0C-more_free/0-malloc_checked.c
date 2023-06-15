4c798bf69ab48b0a395ac284672763f26cbfb369
the-YMB <littemalcolm>
2023-06-15 21:29:08 +0000

    0-malloc_checked.c

---
+++ 0x0C-more_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * malloc_checked - Allocates memory using malloc().
+  * @b: Size of memory to be allocated.
+  *
+  * Return: Return a pointer to the allocated memory.
+  */
+void *malloc_checked(unsigned int b)
+{
+	void *mptr;
+
+	mptr = malloc(b);
+	if (mptr == NULL)
+		exit(98);
+
+	return (mptr);
+}
