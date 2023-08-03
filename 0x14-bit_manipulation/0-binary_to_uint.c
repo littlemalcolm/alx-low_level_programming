fdd8ce183e8ce7c942e7e78b88a5f3c8554e3bed
littlemalcolm <babajideyusuf11@gmail.com>
2023-08-03 13:01:57 +0000

    test-check

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * binary_to_uint - Converts a binary number to base 10.
+  * @b: Character string containing the binary number.
+  *
+  * Return: The equivalent base 10 number.
+  */
+unsigned int binary_to_uint(const char *b)
+{
+	int len = 0, i, pos = 0;
+	unsigned int  b_ten = 0;
+
+	if (!b)
+		return (b_ten);
+
+	while (b[len])
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (b_ten);
+		len++;
+	}
+
+	for (i = len - 1; i >= 0; i--, pos++)
+		b_ten = b_ten + (b[i] - '0') * (1 << pos);
+
+	return (b_ten);
+}
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * get_bit - Gets the bit at a specified index.
+  * @n: Number in which to check bit.
+  * @index: Index of bit to get.
+  *
+  * Return: The value of the bit.
+  */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index < 64)
+	{
+		mask = 1 << index;
+		if (n & mask)
+			return (1);
+		else
+			return (0);
+	}
+	return (-1);
+
+}
